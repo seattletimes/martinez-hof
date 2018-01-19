c0018e3ce66f08ea9678d0cdf26c7304fe4bc301
Thomas Wilburn <news@thomaswilburn.net>
2018-01-19 14:56:41 -0800

    Chart started

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,32 @@
 
 require("component-responsive-frame/child");
 require("component-image-slider");
+
+var $ = require("./lib/qsa");
+
+var Chartist = require("chartist");
+
+var chart = new Chartist.Line(".league-comparison .chart");
+
+var updateChart = function() {
+  var mode = $.one(`input[name="mode"]:checked`).id;
+  console.log(mode);
+  var data = {
+    labels: window.hofData.edgar.map(r => r.season),
+    series: [
+      window.hofData.edgar.map(r => r[mode]),
+      window.hofData.mvps.map(r => r[mode]),
+      window.hofData.league.map(r => r[mode])
+    ]
+  };
+
+  chart.update(data, {
+    lineSmooth: Chartist.Interpolation.none({
+       fillHoles: false
+    })
+  });
+}
+
+updateChart();
+
+$.one(".modes").addEventListener("change", updateChart);
